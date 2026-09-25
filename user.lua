ef0c3028814b8e100e786dacced19466de69e8ee
wilmalinden <wilma.linden@mail.com>
2026-09-25 19:10:17 +0300

    add user.lua

---
+++ user.lua
@@ -0,0 +1,57 @@
+return {
+  {
+    "AstroNvim/astrocore",
+    opts = {
+      mappings = {
+        n = {
+          ["<Up>"] = "<Nop>",
+          ["<Down>"] = "<Nop>",
+          ["<Left>"] = "<Nop>",
+          ["<Right>"] = "<Nop>",
+        },
+        i = {
+          ["<Up>"] = "<Nop>",
+          ["<Down>"] = "<Nop>",
+          ["<Left>"] = "<Nop>",
+          ["<Right>"] = "<Nop>",
+        },
+        v = {
+          ["<Up>"] = "<Nop>",
+          ["<Down>"] = "<Nop>",
+          ["<Left>"] = "<Nop>",
+          ["<Right>"] = "<Nop>",
+        },
+        x = {
+          ["<Up>"] = "<Nop>",
+          ["<Down>"] = "<Nop>",
+          ["<Left>"] = "<Nop>",
+          ["<Right>"] = "<Nop>",
+        },
+        s = {
+          ["<Up>"] = "<Nop>",
+          ["<Down>"] = "<Nop>",
+          ["<Left>"] = "<Nop>",
+          ["<Right>"] = "<Nop>",
+        },
+        o = {
+          ["<Up>"] = "<Nop>",
+          ["<Down>"] = "<Nop>",
+          ["<Left>"] = "<Nop>",
+          ["<Right>"] = "<Nop>",
+        },
+        t = {
+          ["<Up>"] = "<Nop>",
+          ["<Down>"] = "<Nop>",
+          ["<Left>"] = "<Nop>",
+          ["<Right>"] = "<Nop>",
+        },
+        c = {
+          ["<Up>"] = "<Nop>",
+          ["<Down>"] = "<Nop>",
+          ["<Left>"] = "<Nop>",
+          ["<Right>"] = "<Nop>",
+        },
+      },
+    },
+  },
+}
